6e5491240bdc25a15e6ceedebc343397ed900e46
Casey Wylie <cmwylie19@gmail.com>
2022-10-09 11:51:38 +0200

    FEATURE: list files
    Signed-off-by: Casey Wylie <cmwylie19@gmail.com>

---
+++ FEATURE_REQUEST.md
@@ -0,0 +1,19 @@
+# Delete File 
+
+## Prereqs
+
+- watch directory
+- - recursively watch files
+- get files in directory 
+
+
+
+## Todo 
+
+- Model for file
+- - include date of the file
+- - include the name of the file
+
+- delete file by name
+- delete file by time ( time controller )
+- delete files by time frame
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,18 +1,30 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 )
 
-// var wg sync.WaitGroup
+var wg sync.WaitGroup
 
+type File struct {
+	Name string
+	Date string
+}
 type Server struct {
 	Port string
 	Dir  string
 }
+
+// TODO
+// func (s *Server) DeleteFile() error {
+
+// }
 
 // key, cert, port, dir string for future use
 func (s *Server) Serve(key, cert, dir, port string) error {
@@ -21,6 +33,7 @@ func (s *Server) Serve(key, cert, dir, port string) error {
 	s.Dir = dir
 
 	http.HandleFunc("/healthz", HealthCheckHandler)
+	http.HandleFunc("/list", s.ListFiles)
 
 	if key != "" && cert != "" {
 		fmt.Printf("Starting server at %s watching directory %s.\n", port, dir)
@@ -40,6 +53,37 @@ func (s *Server) Serve(key, cert, dir, port string) error {
 		}
 	}
 	return nil
+}
+
+func listFiles(dir string, file_chan chan []File) {
+	//elements := make(map[string]interface{})
+	file_array := []File{}
+	defer wg.Done()
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		file_array = append(file_array, File{
+			Name: file.Name(),
+		})
+	}
+	file_chan <- file_array
+}
+
+// List files in the designated directory
+// Assumes the server has access to the files
+// in Kubernetes via a persistent volume with a hostpath mount
+func (s *Server) ListFiles(w http.ResponseWriter, r *http.Request) {
+	files := make(chan []File)
+	w.Header().Set("Content-Type", "application/json")
+	wg.Add(1)
+
+	go listFiles(s.Dir, files)
+	file_list := <-files
+	wg.Wait()
+	json.NewEncoder(w).Encode(file_list)
 }
 
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
