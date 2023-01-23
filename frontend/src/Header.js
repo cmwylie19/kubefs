d632e7ee32cf04de179a1118598c5cb3297d9e3e
Casey Wylie <cwylie@redhat.com>
2023-01-23 16:10:57 -0500

    [FEATURE] Theme Changer
    Signed-off-by: Casey Wylie <cwylie@redhat.com>

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./Header.css";
+import { ThemeButton } from "./theme";
 
 export const DisplayCount = (setTitle, count) => {
   setTitle("total: " + count);
@@ -8,11 +9,11 @@ export const DisplayCount = (setTitle, count) => {
   }, 1000);
   return "total: " + count;
 };
-export const Header = ({ date, setDate, count, version }) => {
+export const Header = ({ date, setDate, count, version, theme, setTheme }) => {
   const [color, setColor] = useState("Magenta");
   const [title, setTitle] = useState("Kube-FS");
   return (
-    <header className="header">
+    <header className="header" style={{backgroundColor: `${theme === "light" ? "#fff" :"#333"}`}}>
       <div>
         <h1
           role="title"
@@ -26,12 +27,15 @@ export const Header = ({ date, setDate, count, version }) => {
           <div style={{ fontSize: "8pt", color: "grey" }}>{version}</div>
         </h1>
       </div>
+      
       <input
+        style={{backgroundColor: theme === "light" ? "white" : "#333",color: "magenta"}}
         type="text"
         placeholder="Date yymmdd"
         value={date}
         onChange={(e) => setDate(e.target.value)}
       />
+      <ThemeButton theme={theme} setTheme={setTheme} />
     </header>
   );
 };
